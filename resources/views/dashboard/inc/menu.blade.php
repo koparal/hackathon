b4330aa6d168533a091975ea8e0d2846f428e3c4
Saban Koparal <sabankoparal@gmail.com>
2019-12-28 23:16:55 +0300

    .

--- resources/views/dashboard/inc/menu.blade.php
+++ resources/views/dashboard/inc/menu.blade.php
@@ -3,32 +3,32 @@
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link active" href="#">
-                    <span data-feather="home"></span>
                     Dashboard <span class="sr-only">(current)</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{!! route("dashboard.index") !!}">
-                    <span data-feather="file"></span>
                     Kullanıcılar
                 </a>
             </li>
             <li class="nav-item">
+                <a class="nav-link" href="{!! route("dashboard.roles.index") !!}">
+                    Roller
+                </a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="{!! route("dashboard.jobs.index") !!}">
-                    <span data-feather="shopping-cart"></span>
                     İş İlanları
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{!! route("dashboard.news.index") !!}">
-                    <span data-feather="shopping-cart"></span>
                     Haberler
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{!! route("dashboard.events.index") !!}">
-                    <span data-feather="shopping-cart"></span>
-                    Roller
+                    Etkinlikler
                 </a>
             </li>
         </ul>
